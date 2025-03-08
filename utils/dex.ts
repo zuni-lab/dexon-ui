167353c6fcff46a452f461655ab4abbcb84ee5f0
vinhtrand8 <vinhtrand8@gmail.com>
2025-03-08 16:26:03 +0700

    fix: find pools

--- utils/dex.ts
+++ utils/dex.ts
@@ -1,5 +1,6 @@
 import { UNISWAP_V3_POOLS } from "@/constants/contracts";
-import { type Hex, encodePacked } from "viem";
+import { Tokens } from "@/constants/tokens";
+import { type Hex, encodePacked, formatUnits } from "viem";
 
 function isTokenInPool(pool: Pool, token: Hex): boolean {
   return pool.token0.address === token || pool.token1.address === token;
@@ -21,6 +22,7 @@ export function findPaths(tokenIn: Hex, tokenOut: Hex, maxHops = 2): Hex {
     depth: number,
   ) {
     if (currentToken === tokenOut && poolPaths.length > 0) {
+      paths = tokenIn;
       for (let i = 0; i < poolPaths.length; i++) {
         paths = encodePacked(
           ["bytes", "uint24", "address"],
@@ -60,13 +62,49 @@ export function findPaths(tokenIn: Hex, tokenOut: Hex, maxHops = 2): Hex {
     return paths;
   }
 
-  const startToken =
-    startPool.token0.address === tokenIn
-      ? startPool.token0.address
-      : startPool.token1.address;
-
-  paths = startToken;
+  paths = tokenIn;
   findPathRecursive([], [], tokenIn, 0);
 
   return paths;
+}
+
+export function findPoolIds(tokenIn: Hex, tokenOut: Hex): Hex[] {
+  return UNISWAP_V3_POOLS.filter((pool) => {
+    return (
+      (pool.token0.address.toLowerCase() === tokenIn.toLowerCase() &&
+        pool.token1.address.toLowerCase() === tokenOut.toLowerCase()) ||
+      (pool.token0.address.toLowerCase() === tokenOut.toLowerCase() &&
+        pool.token1.address.toLowerCase() === tokenIn.toLowerCase())
+    );
+  }).map((pool) => pool.address);
+}
+
+export function getTokenPairFromPath(path: Hex): string {
+  const addressSize = 40;
+  const firstToken = path.slice(0, addressSize + 2);
+  const lastToken = `0x${path.slice(-addressSize)}`;
+
+  const quoteToken = Tokens.USDC;
+  const baseTokenAddress =
+    firstToken === quoteToken.address ? lastToken : firstToken;
+  const baseToken = Object.values(Tokens).find(
+    (token) => token.address === baseTokenAddress,
+  );
+  if (!baseToken) {
+    throw new Error("Token not found");
+  }
+
+  return `${baseToken.symbol}/${quoteToken.symbol}`;
+}
+
+export function formatAmountWithPath(path: Hex, amount: bigint): string {
+  const firstToken = path.slice(0, 42);
+  const token = Object.values(Tokens).find(
+    (token) => token.address === firstToken,
+  );
+  if (!token) {
+    throw new Error("Token not found");
+  }
+
+  return formatUnits(amount, token.decimals);
 }

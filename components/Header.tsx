c47f7f55b896c88b006d40128560000ec5afa2f4
vinhtrand8 <vinhtrand8@gmail.com>
2025-03-12 21:40:44 +0700

    feat: faucet

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,13 @@
+"use client";
+
 import { Button } from "@/components/shadcn/Button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/shadcn/Dialog";
 import {
   Tooltip,
   TooltipContent,
@@ -6,18 +15,68 @@ import {
   TooltipTrigger,
 } from "@/components/shadcn/Tooltip"; // Add these imports
 
+import { FAUCET_ADDRESS } from "@/constants/contracts";
 import Link from "next/link";
+import { useState } from "react";
+import { useWriteContract } from "wagmi";
 import ConnectWallet from "./ConnectWallet";
 import { LogoSvg } from "./icons/LogoSvg";
 import { SettingIcon } from "./icons/Setting";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const { writeContract } = useWriteContract({
+    mutation: {
+      onSuccess: () => setOpen(false),
+    },
+  });
+
   return (
     <header className="flex w-full items-center justify-between">
       <Link href="/">
         <LogoSvg />
       </Link>
       <div className="flex items-center gap-3">
+        <Dialog open={open}>
+          <DialogTrigger asChild>
+            <Button
+              className="h-12 rounded-full bg-primary px-6 text-gray-100 text-lg hover:bg-primary/90"
+              onClick={() => setOpen(true)}
+            >
+              Faucet
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="border-0 bg-secondary text-center sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>You will receive</DialogTitle>
+            </DialogHeader>
+            <div className="grid gap-4">
+              <div className="font-medium">10 WBTC</div>
+              <div className="font-medium">100 WETH</div>
+              <div className="font-medium">1,000 WSOL</div>
+              <div className="font-medium">1,000,000 USDC</div>
+              <div>
+                <Button
+                  className="rounded-full bg-purple4/60 px-4 py-5 font-medium hover:bg-purple4/50"
+                  onClick={() =>
+                    writeContract({
+                      abi: [
+                        {
+                          name: "faucet",
+                          type: "function",
+                        },
+                      ],
+                      address: FAUCET_ADDRESS,
+                      functionName: "faucet",
+                    })
+                  }
+                >
+                  Faucet
+                </Button>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger asChild>
